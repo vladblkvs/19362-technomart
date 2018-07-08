e9741aaca83a66eec99a36af1b09731abab58416
vladblkvs <gremlinux@gmail.com>
2018-07-08 14:52:21 +0300

    Добавлена проверка вызова форм

--- js/script.js
+++ js/script.js
@@ -1,89 +1,101 @@
 var buyLinkArr = document.querySelectorAll(".buy");
-var buyPopup = document.querySelector(".modal-cart");
-var buyClose = buyPopup.querySelector(".modal-close");
 
-for (var i = 0; i < buyLinkArr.length; i++) {
-  buyLinkArr[i].addEventListener("click", function(evt) {
+if (buyLinkArr) {
+  var buyPopup = document.querySelector(".modal-cart");
+  var buyClose = buyPopup.querySelector(".modal-close");
+
+  for (var i = 0; i < buyLinkArr.length; i++) {
+    buyLinkArr[i].addEventListener("click", function(evt) {
+      evt.preventDefault();
+      buyPopup.classList.add("modal-show");
+    });
+  }
+
+  buyClose.addEventListener("click", function(evt) {
     evt.preventDefault();
-    buyPopup.classList.add("modal-show");
+    buyPopup.classList.remove("modal-show");
   });
 }
 
-buyClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  buyPopup.classList.remove("modal-show");
-});
-
 var mapLink = document.querySelector(".contacts-map");
-var mapPopup = document.querySelector(".modal-map");
-var mapClose = mapPopup.querySelector(".modal-close");
 
-mapLink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
+if (mapLink) {
+  var mapPopup = document.querySelector(".modal-map");
+  var mapClose = mapPopup.querySelector(".modal-close");
 
-mapClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
-});
+  mapLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
 
-var writeLink = document.querySelector(".write-us");
-var writePopup = document.querySelector(".modal-write-us");
-var writeClose = writePopup.querySelector(".modal-close");
-var form = writePopup.querySelector(".contact-form");
-var clientName = writePopup.querySelector("[name=client-name]");
-var email = writePopup.querySelector("[name=email]");
-var message = writePopup.querySelector("[name=message]");
-
-var isStorageSupport = true;
-var storage = "";
-var storage1 = "";
-try {
-  storage = localStorage.getItem("client-name");
-  storage1 = localStorage.getItem("email");
-} catch (err) {
-  isStorageSupport = false;
+  mapClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
 }
 
-writeLink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  writePopup.classList.add("modal-show");
-  if (storage) {
-    clientName.value = storage;
-    email.value = storage1;
-    message.focus();
-  } else {
-    clientName.focus();
+var writeLink = document.querySelector(".write-us");
+
+if (writeLink) {
+  var writePopup = document.querySelector(".modal-write-us");
+  var writeClose = writePopup.querySelector(".modal-close");
+  var form = writePopup.querySelector(".contact-form");
+  var clientName = writePopup.querySelector("[name=client-name]");
+  var email = writePopup.querySelector("[name=email]");
+  var message = writePopup.querySelector("[name=message]");
+
+  var isStorageSupport = true;
+  var storage = "";
+  var storage1 = "";
+  try {
+    storage = localStorage.getItem("client-name");
+    storage1 = localStorage.getItem("email");
+  } catch (err) {
+    isStorageSupport = false;
   }
-});
 
-writeClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  writePopup.classList.remove("modal-show");
-});
-
-form.addEventListener("submit", function(evt) {
-  if (!clientName.value || !email.value || !message.value) {
+  writeLink.addEventListener("click", function(evt) {
     evt.preventDefault();
-    console.log("Нужно ввести имя, адрес электронной почты и текст сообщения.");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("client-name", clientName.value);
-      localStorage.setItem("email", email.value);
+    writePopup.classList.add("modal-show");
+    if (storage) {
+      clientName.value = storage;
+      email.value = storage1;
+      message.focus();
+    } else {
+      clientName.focus();
     }
-  }
-});
+  });
+
+  writeClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    writePopup.classList.remove("modal-show");
+  });
+
+  form.addEventListener("submit", function(evt) {
+    if (!clientName.value || !email.value || !message.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести имя, адрес электронной почты и текст сообщения.");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("client-name", clientName.value);
+        localStorage.setItem("email", email.value);
+      }
+    }
+  });
+}
 
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     if (mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
       mapPopup.classList.remove("modal-show");
     }
     if (writePopup.classList.contains("modal-show")) {
+      evt.preventDefault();
       writePopup.classList.remove("modal-show");
     }
     if (buyPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
       buyPopup.classList.remove("modal-show");
     }
   }
